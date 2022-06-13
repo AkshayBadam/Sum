feaf4e7635a0ea907bd6a46f46bc44acca41f032
Akshay Badam <Akshay_Badam@epam.com>
2022-06-13 23:03:46 +0530

    Add project files.

---
+++ Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+
+namespace Addition
+{
+    class Myclass
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 5;
+            int sum = x + y;
+            Console.WriteLine(sum);
+
+
+
+
+        }
+    }
+}
